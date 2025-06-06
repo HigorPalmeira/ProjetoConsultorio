3da36c8a5f53f84ac8bb08c22f5e9d4765a712cf
Higor Palmeira <higorpresente05@gmail.com>
2025-06-06 18:11:31 -0300

    Refatorando o novo EspecialidadeController.

--- Consultorio/src/main/java/higorpalmeira/com/github/consultorio/controller/EspecialidadeController.java
+++ Consultorio/src/main/java/higorpalmeira/com/github/consultorio/controller/EspecialidadeController.java
@@ -4,15 +4,9 @@
  */
 package main.java.higorpalmeira.com.github.consultorio.controller;
 
-import java.awt.Frame;
 import java.util.List;
-import java.util.Scanner;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
 import main.java.higorpalmeira.com.github.consultorio.model.entity.Especialidade;
-import main.java.higorpalmeira.com.github.consultorio.service.EspecialidadeService;
+import main.java.higorpalmeira.com.github.consultorio.service.IEspecialidadeService;
 
 /**
  *
@@ -20,163 +14,72 @@ import main.java.higorpalmeira.com.github.consultorio.service.EspecialidadeServi
  */
 public class EspecialidadeController {
     
-    public static void criarEspecialidade(String descricao, JFrame frmOrigem) {
+    private final IEspecialidadeService especialidadeService;
+    
+    public EspecialidadeController(IEspecialidadeService especialidadeService) {
+        this.especialidadeService = especialidadeService;
+    }
+    
+    public void criarEspecialidade(String descricao) {
         
-        if (frmOrigem != null) {
-            if (EspecialidadeService.insert(descricao)) {
-                JOptionPane.showMessageDialog(null, "Especialidade criada com sucesso!", "Sucesso", 1);
-
-                if (frmOrigem.getName().equals("frmCadEspecialidade")) {
-                    frmOrigem.dispose();
-                }
-
-            } else {
-                JOptionPane.showMessageDialog(null, "Especialidade não pode ser criada!", "Falha na criação", 2);
-            }
+        if (this.especialidadeService.criarEspecialidade(descricao)) {
+            System.out.println("Especialidade criada com sucesso!");
             
         } else {
-            
-            if (EspecialidadeService.insert(descricao)) {
-                System.out.println("Especialidade criada com sucesso!");
-            } else {
-                System.out.println("Especialidade não pode ser criada!");
-            }
-            
+            System.out.println("Especialidade não pode ser criada!");
         }
-        
         
     }
     
-    public static void editarEspecialidade(int id, String currentDescricao, String novaDescricao) {
+    public void atualizarEspecialidade(int id, String descricao) {
         
-        Frame[] frames = JFrame.getFrames();
-        
-        if (frames.length > 0) {
-            if ( currentDescricao.equals(novaDescricao) ) {
-                JOptionPane.showMessageDialog(null, "Para editar a especialidade '" + novaDescricao + "' coloque um nome diferente da atual!", "Edição Inválida!", 2);
-                return;
-            }
-
-            if (EspecialidadeService.update(id, novaDescricao)) {
-                JOptionPane.showMessageDialog(null, "Especialidade editada com sucesso!", "Edição Concluída!", 1);
-
-            }
-        } else {
-            if ( EspecialidadeService.selectId(id)
-                                        .getDescricao().equals(novaDescricao) ) {
-                
-                System.out.println("\n===\tEdição Inválida!\t===");
-                System.out.println("Para editar a especialidade '" + novaDescricao + "' coloque um nome diferente da atual!\n");
-                return;
-                
-            }
-
-            if (EspecialidadeService.update(id, novaDescricao)) {
-                System.out.println("\n===\tEdição Concluída!\t===");
-                System.out.println("Especialidade editada com sucesso!\n");
-
-            }
-        }
-        
-        
-    }
-    
-    public static void excluirEspecialidade(int id, String descricao) {
-        
-        Frame[] frames = JFrame.getFrames();
-        
-        if (frames.length > 0) {
-            if ( JOptionPane.showConfirmDialog(null, 
-                                            "Tem certeza de que deseja excluir a especialidade '" 
-                                                    + descricao + "'?", 
-                                            "Tem certeza?", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
-
-                if (EspecialidadeService.delete(id)) {
-                    JOptionPane.showMessageDialog(null, "A especialidade foi excluída com sucesso!", "Exclusão Concluída!", 1);
-                }
-
-            }
+        if (this.especialidadeService.atualizarEspecialidade(id, descricao)) {
+            System.out.println("Especialidade atualizada com sucesso!");
             
         } else {
-            Scanner s = new Scanner(System.in);
-            System.out.println("Tem certeza de que deseja excluir a especialidade '" + EspecialidadeService.selectId(id).getDescricao() + "'? [Y/N] " );
-            
-            if (s.next().trim().toUpperCase().equals("Y")) {
-                
-                if (EspecialidadeService.delete(id)) {
-                    System.out.println("\n===\tExclusão Concluída!\t===");
-                    System.out.println("A especialidade foi excluída com sucesso!\n");
-                }
-                
-            } else {
-                System.out.println("Exclusão cancelada!\n");
-            }
+            System.out.println("Especialidade não pode ser atualizada!");
         }
         
     }
     
-    public static void listarEspecialidade(int id, JTable tabelaVisualizar) {
+    public void deletarEspecialidade(int id) {
         
-        Especialidade especialidade = EspecialidadeService.selectId(id);
-        
-        if (tabelaVisualizar == null) {
-            
-            if (especialidade.getId() == 0 && especialidade.getDescricao() == null) {
-                System.out.printf("A especialidade com ID '%d' não foi encontrada no sistema!\n", id);
-
-            } else {
-                System.out.println("===\tEspecialidade\t===");
-                System.out.println(especialidade.toString());
-            }
+        if (this.especialidadeService.deletarEspecialidade(id)) {
+            System.out.println("Especialidade deletada com sucesso!");
             
         } else {
-            
-            if (especialidade == null) {
-                JOptionPane.showMessageDialog(null, 
-                        "A especialidade com o ID '" + id + "' não foi encontrada no sistema!", "Não encontrada!", 1);
-                
-            } else {
-                DefaultTableModel dtmEspecialidade = (DefaultTableModel) tabelaVisualizar.getModel();
-                dtmEspecialidade.setRowCount(0);
-                Object[] especialidadeObj = { especialidade.getId(), especialidade.getDescricao() };
-                dtmEspecialidade.addRow(especialidadeObj);
-            }
-        }
-        
-        
-    }
-    
-    public static void listarEspecialidades(JTable tabelaVisualizar) {
-        
-        DefaultTableModel dtmEspecialidades = (DefaultTableModel) tabelaVisualizar.getModel();
-        List<Especialidade> listaEspecialidades = EspecialidadeService.selectAll();
-
-        dtmEspecialidades.setRowCount(0);
-
-        for (Especialidade especialidade : listaEspecialidades) {
-            Object[] especialidades = { especialidade.getId(), especialidade.getDescricao() };
-            dtmEspecialidades.addRow(especialidades);
+            System.out.println("Especialidade não pode ser deletada!");
         }
         
     }
     
-    public static void listarEspecialidades() {
+    public void listarTodasEspecialidades() {
         
-        List<Especialidade> listaEspecialidades = EspecialidadeService.selectAll();
+        List<Especialidade> listaEspecialidades = this.especialidadeService.listarTodasEspecialidades();
         
         if (listaEspecialidades.isEmpty()) {
             System.out.println("Não há especialidades cadastradas no sistema!");
             
         } else {
-            System.out.println("===\tLista de Especialidades\t===");
-            
             for (Especialidade especialidade : listaEspecialidades) {
                 System.out.println(especialidade.toString());
             }
             
-            System.out.println("===\tFim da Lista\t===");
         }
         
+    }
+    
+    public void buscarEspecialidadePorId(int id) {
+        
+        Especialidade especialidade = this.especialidadeService.buscarEspecialidadePorId(id);
+        
+        if (especialidade == null || (especialidade.getId() == 0 && especialidade.getDescricao() == null) ) {
+            System.out.println("Especialidade não encontrada no sistema!");
+            
+        } else {
+            System.out.println(especialidade.toString());
+            
+        }
     }
     
 }

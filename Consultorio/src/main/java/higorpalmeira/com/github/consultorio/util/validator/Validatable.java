a459d7efcd162ff36f8e2c0b19fe3c47872bf6ed
Higor Palmeira <higorpresente05@gmail.com>
2025-06-06 11:41:54 -0300

    Criando interface de validação.

---
+++ Consultorio/src/main/java/higorpalmeira/com/github/consultorio/util/validator/Validatable.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package main.java.higorpalmeira.com.github.consultorio.util.validator;
+
+/**
+ *
+ * @author higor
+ */
+public interface Validatable {
+    
+    public boolean isCpf(String cpf);
+    
+    public boolean isEmail(String email);
+    
+}

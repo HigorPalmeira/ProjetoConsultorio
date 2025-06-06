cdd2fa3a1125e02b03818f07ee7a925a218e1566
Higor Palmeira <higorpresente05@gmail.com>
2025-06-06 18:18:23 -0300

    Criando classe Medico.

---
+++ Consultorio/src/main/java/higorpalmeira/com/github/consultorio/model/entity/Medico.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.java.higorpalmeira.com.github.consultorio.model.entity;
+
+/**
+ *
+ * @author higor
+ */
+public class Medico {
+    private int id;
+    private String nome;
+    private String crm;
+    private Especialidade especialidade;
+    private String telefone;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+
+    public Especialidade getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(Especialidade especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Medico{" + "id=" + id + ", nome=" + nome + ", crm=" + crm + ", especialidade=" + especialidade + ", telefone=" + telefone + ", email=" + email + '}';
+    }
+    
+}

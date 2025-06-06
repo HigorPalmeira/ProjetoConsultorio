050c29146c4cb8e4ee576d923e470242defba7f7
Higor Palmeira <higorpresente05@gmail.com>
2025-06-06 12:27:32 -0300

    Implementação da interface IPacienteService na classe PacienteServiceImpl.

---
+++ Consultorio/src/main/java/higorpalmeira/com/github/consultorio/service/PacienteServiceImpl.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.java.higorpalmeira.com.github.consultorio.service;
+
+import java.util.Date;
+import java.util.List;
+import main.java.higorpalmeira.com.github.consultorio.model.dao.PacienteDAO;
+import main.java.higorpalmeira.com.github.consultorio.model.entity.Paciente;
+
+/**
+ *
+ * @author higor
+ */
+public class PacienteServiceImpl implements IPacienteService {
+    
+    private final PacienteDAO pacienteDAO;
+    
+    public PacienteServiceImpl(PacienteDAO pacienteDAO) {
+        this.pacienteDAO = pacienteDAO;
+    }
+
+    @Override
+    public boolean criarPaciente(String nome, String cpf, Date dataNascimento, String telefone, String email) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean atualizarPaciente(int id, String nome, String cpf, Date dataNascimento, String telefone, String email) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean deletarPaciente(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Paciente> listarTodosPaciente() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Paciente buscarPacientePorId(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+}

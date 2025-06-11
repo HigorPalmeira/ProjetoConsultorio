b5c676ffb02dd1b737283aca88028d39bc9a3a3d
Higor Palmeira <higorpresente05@gmail.com>
2025-06-11 16:49:41 -0300

    Criando classe service do Endereco.

---
+++ Consultorio/src/main/java/higorpalmeira/com/github/consultorio/service/EnderecoServiceImpl.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.java.higorpalmeira.com.github.consultorio.service;
+
+import java.util.List;
+import main.java.higorpalmeira.com.github.consultorio.model.entity.Endereco;
+
+/**
+ *
+ * @author higor
+ */
+public class EnderecoServiceImpl implements IEnderecoService {
+
+    @Override
+    public boolean criarEndereco(String rua, String numero, String bairro, String cidade, String estado, String cep) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean atualizarEndereco(int id, String rua, String numero, String bairro, String cidade, String estado, String cep) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean deletarEndereco(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Endereco> listarTodosEnderecos() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Endereco buscarEnderecoPorId(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
